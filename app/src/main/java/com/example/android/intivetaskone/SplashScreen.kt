dfbb0e91e0475f3f0ac61ccebb0a1955864ccaef
Konrad <kmaslowiec@gmail.com>
2021-11-23 21:49:25 +0100

    Splash Screen 0.8 Deprecated code has been removed

--- app/src/main/java/com/example/android/intivetaskone/SplashScreen.kt
+++ app/src/main/java/com/example/android/intivetaskone/SplashScreen.kt
@@ -2,26 +2,31 @@ package com.example.android.intivetaskone
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.WindowManager
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
+//The warning is regarding Splash Screen Api
 @SuppressLint("CustomSplashScreen")
-@Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        setContentView(R.layout.activity_splash_screen)
+        //Hides Status and App/Action bar. Shows back button when the screen is touched
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat
+                .BEHAVIOR_SHOW_BARS_BY_TOUCH
+        }
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-        Handler().postDelayed({
+        //Directs to start activity after 5 second
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
